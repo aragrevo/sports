e98ce5fdfb17bde9d0e5d042f89050441ac80441
aragrevo <eduver_san@hotmail.com>
2021-04-02 20:10:51 -0500

    test2

--- pages/api/sports.js
+++ pages/api/sports.js
@@ -6,18 +6,25 @@ export default async (req, res) => {
   const { date, sports } = JSON.parse(req.body);
   console.log(date, sports);
 
-  const data = sports.map((sport) => {
-    const tmpData = getMatchesByDate(
-      date,
-      sport,
-      [],
-      `/es/calendar?date=${date}&sport_code=${sport}`
-    );
-    return tmpData;
-  });
+  // const data = sports.map((sport) => {
+  //   const tmpData = getMatchesByDate(
+  //     date,
+  //     sport,
+  //     [],
+  //     `/es/calendar?date=${date}&sport_code=${sport}`
+  //   );
+  //   return tmpData;
+  // });
+  const sport = sports[0];
+  const data = await getMatchesByDate(
+    date,
+    sport,
+    [],
+    `/es/calendar?date=${date}&sport_code=${sport}`
+  );
 
   // Promise.all(data).then((resp) => {
   //   res.status(200).json({ data: resp });
   // });
-  res.status(200).json({ data: 'ok' });
+  res.status(200).json({ data: data });
 };
